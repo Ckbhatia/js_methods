0d008c627f042a64cd9c9579f8313e05e51124a0
Chetan Kumar <chetansain86@gmail.com>
2019-07-01 23:28:07 +0530

    complete 05 assignments of array methods

--- array-methods/5-object.js
+++ array-methods/5-object.js
@@ -1,4 +1,7 @@
 // 1. Write a JavaScript program to list the properties and values of a JavaScript object. (Object.keys)
+function listProp(object, val) {
+  return val === 'val' ? Object.values(object) : Object.keys(student);
+}
 
 
 
@@ -8,8 +11,16 @@ var student = {
   sclass : "VI", 
   rollno : 12
 };
+delete student.rollno;
 
 
 
 
 // 3. Write a function to get the length of an object.
+let length = 0;
+function getLength(object) {
+    for (let key in object) {
+		length = length + key.length;
+	}
+  return length;
+};
