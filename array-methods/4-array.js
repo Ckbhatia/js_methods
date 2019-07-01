77138946c0ed10de7dd8d0ed74abd2abc9768685
Chetan Kumar <chetansain86@gmail.com>
2019-07-01 23:27:33 +0530

    complete 04 assignments of array methods

--- array-methods/4-array.js
+++ array-methods/4-array.js
@@ -29,6 +29,13 @@ var data = [
 // write a function that will sum all of the dogs ages in dog years using for loop.
 // 1 human year = 7 dog year
 // Solution is 105
+let sum = 0;
+for (let k = 0; k < data.length; k++) {
+  if (data[k].type === 'dog') {
+    sum = sum + data[k].age;
+  }
+}
+sum *= 7;
 
 
 // Write the same function using
@@ -36,3 +43,19 @@ var data = [
 // 2. map - to multiply human year to dog year
 // 3. reduce - to accumulate total age.
 // Solution 105
+function filter(type) {
+  return data.filter(item => item.type === type);
+}
+
+function convertToDogAge(humanAge) {
+  return data.map(item => item.type === 'dog' ? item.age = 7 * humanAge : item.age);
+}
+
+function totalAge() {
+  return data.reduce((acc, item) => {
+    if(item.type === "dog"){
+      acc = acc + item.age;
+    }
+    return acc; 
+  }, 0);
+};
